86d663f42ab5dc2a1021be3d10c03283765714f8
datafaust <faustolopez110@gmail.com>
2021-02-10 14:34:13 -0500

    adding files

---
+++ activateMic.py
@@ -0,0 +1,15 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
+
+def micStart():
+    print('start recording....')
+    fs = 44100  # Sample rate
+    seconds = 7  # Duration of recording
+    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+    sd.wait()  # Wait until recording is finished
+    #write('/mp3/recording.wav', fs, myrecording)  # Save as WAV file 
+    print('ended recording and wrote out!')
+
+
+if __name__ == '__main__':
+    micStart()
---
+++ recordAndRecognize.py
@@ -0,0 +1,6 @@
+import activateMic
+
+if __name__ == '__main__':
+    # service.py executed as script
+    # do something
+    activateMic.micStart()
