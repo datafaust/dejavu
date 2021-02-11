6d38eba657f3eff26bc45a73f39ebe1582ea9b40
datafaust <faustolopez110@gmail.com>
2021-02-11 10:15:51 -0500

    adding files

--- activateMic.py
+++ activateMic.py
@@ -7,7 +7,7 @@ def micStart():
     seconds = 7  # Duration of recording
     myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
     sd.wait()  # Wait until recording is finished
-    #write('/mp3/recording.wav', fs, myrecording)  # Save as WAV file 
+    write('mp3/recording.wav', fs, myrecording)  # Save as WAV file 
     print('ended recording and wrote out!')
 
 
--- recordAndRecognize.py
+++ recordAndRecognize.py
@@ -1,6 +1,33 @@
 import activateMic
+import json
+from dejavu import Dejavu
+from dejavu.logic.recognizer.file_recognizer import FileRecognizer
+from dejavu.logic.recognizer.microphone_recognizer import MicrophoneRecognizer
 
-if __name__ == '__main__':
+# load config from a JSON file (or anything outputting a python dictionary)
+config = {
+    "database": {
+        "host": "db",
+        "user": "postgres",
+        "password": "password",
+        "database": "dejavu"
+    },
+    "database_type": "postgres"
+}
+
+def runDejavu():
+    # create a Dejavu instance
+    djv = Dejavu(config)
+
     # service.py executed as script
     # do something
     activateMic.micStart()
+    results = djv.recognize(FileRecognizer, "mp3/recording.wav")
+    print(f"From file we recognized: {results}\n")
+
+
+if __name__ == '__main__':
+    runDejavu()
+    
+    
+
